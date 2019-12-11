745d2d80516a2ecd94ab99c0179fc8cadfa26ba2
Radouane Bahi <radbahi@gmail.com>
2019-12-11 21:10:04 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
 # code here!
+class School
+  attr_accessor :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+   roster[grade] ||= []
+    @roster[grade] << name
+  end
+  
+end #end of class School
